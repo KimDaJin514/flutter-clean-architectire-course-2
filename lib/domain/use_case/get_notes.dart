0c53d152abea894dfc388ffb01b52df61094ecc3
KimDaJin <kimdajin514@gmail.com>
2024-03-26 14:03:50 +0900

    feat: add use case

---
+++ lib/domain/use_case/get_notes.dart
@@ -0,0 +1,12 @@
+import 'package:note_app/domain/model/note.dart';
+import 'package:note_app/domain/respository/note_repository.dart';
+
+class GetNotes {
+  final NoteRepository repository;
+  GetNotes(this.repository);
+
+  Future<List<Note>> call() async {
+    List<Note> notes = await repository.getNotes();
+    return notes;
+  }
+}
